48495554b537c56966beac9956de325f4489c3e6
Arthur-Pereira <60709850+Arthur-Pereira-5019@users.noreply.github.com>
2025-08-01 19:17:36 -0300

    Simple rest endpoint

---
+++ First-REST-Endpoint/src/main/java/com/arthur_pereira/First/REST/Endpoint/FirstRestEndpointApplication.java
@@ -0,0 +1,13 @@
+package com.arthur_pereira.First.REST.Endpoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstRestEndpointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstRestEndpointApplication.class, args);
+	}
+
+}
