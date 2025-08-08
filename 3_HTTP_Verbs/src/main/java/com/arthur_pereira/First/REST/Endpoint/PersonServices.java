d68421f019e0179b8d4dc893fef184919c5637dd
Arthur-Pereira <60709850+Arthur-Pereira-5019@users.noreply.github.com>
2025-08-08 17:58:34 -0300

    Finalized the session about HTTP verbs, using mocks and Postman to test the application endpoints

--- 3_HTTP_Verbs/src/main/java/com/arthur_pereira/First/REST/Endpoint/PersonServices.java
+++ 3_HTTP_Verbs/src/main/java/com/arthur_pereira/First/REST/Endpoint/PersonServices.java
@@ -5,6 +5,7 @@ import com.arthur_pereira.First.REST.Endpoint.model.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
@@ -15,15 +16,48 @@ public class PersonServices {
     private final AtomicLong counter = new AtomicLong();
     private Logger logger = Logger.getLogger(PersonServices.class.getName());
 
+
+
+    public ArrayList<Person> findAll() {
+        logger.info("Finding all people");
+        ArrayList<Person> persons = new ArrayList<Person>();
+
+        for(int i = 0; i < 8; i++) {
+            persons.add(mockPerson(i));
+        }
+        return persons;
+    }
+
+    private Person mockPerson(Integer i) {
+        if (i == null) {
+            i = 0;
+        }
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setAdress("SC " + i);
+        person.setFirstName("Arthur " + i);
+        person.setLastName("Pereira " + i);
+        person.setGender("M "+i );
+        return person;
+    }
+
+    public Person createPerson(Person person) {
+        logger.info("Creating new person");
+        return person;
+    }
+
+    public Person updatePerson(Person person) {
+        logger.info("Updating a person");
+        return person;
+    }
+
+    public void deletePerson(String id) {
+        logger.info("Deleted a person!");
+    }
+
     public Person findById(String id) {
         logger.info("Finding one person!");
 
-        Person person = new Person();
-        person.setId(counter.incrementAndGet());
-        person.setAdress("SC");
-        person.setFirstName("Arthur");
-        person.setLastName("Pereira");
-        person.setGender("M");
-        return person;
+        return mockPerson(0);
     }
 }
--- 3_HTTP_Verbs/src/main/java/com/arthur_pereira/First/REST/Endpoint/controllers/PersonController.java
+++ 3_HTTP_Verbs/src/main/java/com/arthur_pereira/First/REST/Endpoint/controllers/PersonController.java
@@ -4,10 +4,9 @@ import com.arthur_pereira.First.REST.Endpoint.PersonServices;
 import com.arthur_pereira.First.REST.Endpoint.model.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 
 @RestController
 @RequestMapping("/person")
@@ -19,6 +18,30 @@ public class PersonController {
     @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public Person findById(@PathVariable("id") String id) {
         return service.findById(id);
+    }
+
+    @RequestMapping("/all")
+    public ArrayList<Person> findAll() {
+        return service.findAll();
+    }
+
+    @RequestMapping(value = "",method = RequestMethod.POST,
+            produces = MediaType.APPLICATION_JSON_VALUE,
+            consumes = MediaType.APPLICATION_JSON_VALUE)
+    public Person create(@RequestBody Person person) {
+        return service.createPerson(person);
+    }
+
+    @RequestMapping(value = "",method = RequestMethod.PUT,
+            produces = MediaType.APPLICATION_JSON_VALUE,
+            consumes = MediaType.APPLICATION_JSON_VALUE)
+    public Person update(@RequestBody Person person) {
+        return service.updatePerson(person);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public void delete(@PathVariable("id") String id) {
+        service.deletePerson(id);
     }
 
 }
