1d5396f0818a27c091cd7a31d2aa6fd659330a0f
Arthur-Pereira <60709850+Arthur-Pereira-5019@users.noreply.github.com>
2025-09-28 13:49:43 -0300

    Fixed project name structure as branches now will be used, started course section about CORS configuration

--- 17_Integration_Tests/src/main/java/com/arthur_pereira/custom_json_serialization/repositories/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.arthur_pereira.custom_json_serialization.repositories;
-
-import com.arthur_pereira.custom_json_serialization.model.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<Book, Long> {
-}
--- 17_Integration_Tests/src/test/java/com/arthur_pereira/custom_json_serialization/config/TestConfigs.java
+++
@@ -1,5 +0,0 @@
-package com.arthur_pereira.custom_json_serialization.config;
-
-public interface TestConfigs {
-    int SERVER_PORT = 8888;
-}
